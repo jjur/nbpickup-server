64147ece850743528bd9ad8b6933021bd575fa71
jjur <juraj.vasek@minerva.kgi.edu>
2021-09-28 11:48:42 +0100

    I have no clue what changed

--- app/Views/frontend/header.php
+++ app/Views/frontend/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="/assets/css/vanilla-zoom.min.css">
 
     <!-- Falvicons -->
+    <!-- TODO: https://catalin.red/svg-favicon-light-dark-theme/ -->
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
