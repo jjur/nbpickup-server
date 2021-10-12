db0aecb34184596274b5e6d45282f67e8a84153b
jjur <juraj.vasek@minerva.kgi.edu>
2021-10-13 00:06:30 +0100

    Login Page

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,31 +1,34 @@
 <?php echo view("frontend/header"); ?>
-<h1><?php echo lang('Auth.login_heading');?></h1>
-<p><?php echo lang('Auth.login_subheading');?></p>
+<main class="page login-page">
+    <section class="clean-block clean-form dark">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-info"><?php echo lang('Auth.login_heading');?></h2>
+                <p><?php echo lang('Auth.login_subheading');?></p>
+                <div id="infoMessage"><?php echo $message;?></div>
+            </div>
+            <?php echo form_open('auth/login');?>
+                <div class="mb-3"><label class="form-label" for="identity"><?= lang('Auth.login_identity_label')?></label><input class="form-control item" name="identity" type="email" id="identity"></div>
+                <div class="mb-3"><label class="form-label" for="password"><?= lang('Auth.login_password_label')?></label><input class="form-control" type="password" name="password" id="password"></div>
+                <div class="mb-3">
+                    <div class="form-check"><input class="form-check-input" name="remember" type="checkbox" id="checkbox"><label class="form-check-label" for="checkbox"><?= lang('Auth.login_remember_label')?></label></div>
+                </div><button class="btn btn-primary" type="submit"><?= lang('Auth.login_submit_btn')?></button>
+            <br><br>
+            <p class="small"><a href="forgot_password"><?php echo lang('Auth.login_forgot_password');?></a></p>
+            <?php echo form_close();?>
 
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open('auth/login');?>
-
-  <p>
-    <?php echo form_label(lang('Auth.login_identity_label'), 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo form_label(lang('Auth.login_password_label'), 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo form_label(lang('Auth.login_remember_label'), 'remember');?>
-    <?php echo form_checkbox('remember', '1', false, 'id="remember"');?>
-  </p>
+        </div>
+    </section>
+</main>
 
 
-  <p><?php echo form_submit('submit', lang('Auth.login_submit_btn'));?></p>
 
-<?php echo form_close();?>
 
-<p><a href="forgot_password"><?php echo lang('Auth.login_forgot_password');?></a></p>
+
+
+
+
+
+
 
 <?php echo view('frontend/footer');?>
