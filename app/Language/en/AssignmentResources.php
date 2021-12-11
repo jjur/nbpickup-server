204b202f5c4e970040cbb57f0b732313e6faa906
jjur <juraj.vasek@minerva.kgi.edu>
2021-12-11 02:42:43 +0000

    Language file for Assignment Resources page

---
+++ app/Language/en/AssignmentResources.php
@@ -0,0 +1,25 @@
+<?php
+    return [
+        'page_title' => 'Assignment Resources',
+        'gradebook_box_title' => 'Files - Gradebook.db',
+        'upload_gradebook' => 'Upload New Gradebook.db file',
+        'select_file' => 'Choose file',
+        'upload' => 'Upload',
+        'timestamp' => 'Timestamp',
+        'size' => 'Size',
+        'additional_details' => '...',
+        'gradebook_history' => 'Gradebook History',
+
+        'files_box_title' => 'Assignment Files',
+        'filename' => 'File Name',
+
+        "authoring_box_title" => "Edit Assignment Online",
+        "authoring_info" => "Online Jupyter Notebook environment will appear in new tab. After website loads, please follow the instructions in the notebook.",
+
+        "preview_box_title" =>"Preview Assignment for Students",
+        "preview_info" => "TODO",
+
+        'save' => 'Save & Next Page',
+        'back' => 'Back',
+        #'' => '',
+    ];
