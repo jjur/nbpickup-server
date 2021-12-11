644149f9150f07282142710f205e7b5d51f28439
jjur <juraj.vasek@minerva.kgi.edu>
2021-12-11 02:38:14 +0000

    Language file for Assingment Settings page

---
+++ app/Language/en/AssignmentSettings.php
@@ -0,0 +1,21 @@
+<?php
+    return [
+        'page_title' => 'Assignment Submission Settings',
+        'card_submit_jupyter' => 'Submit Using Jupyter Notebook library',
+        'card_submit_jupyter_instructions' => 'Lorem Ipsum',
+        'insert_email_caps' => 'INSERT_YOUR_EMAIL',
+
+        'card_submit_web' => 'Submit Using Web Portal',
+        'card_submit_web_instructions' => 'In Development',
+        'submission_url' => 'Submission URL:',
+
+        'card_submit_email' => 'Submit Using Automatic Email',
+        'card_submit_email_instructions' => 'In Development',
+        'subject_field' => 'Insert following name in Email Subject Field:',
+        'email_address' => 'Use following Email address',
+
+
+        'save' => 'Save & Next Page',
+        'cancel' => 'Cancel',
+        #'' => '',
+    ];
