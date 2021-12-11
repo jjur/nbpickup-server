e59c0cd8b8f7f7eb0f18796275ce9f61d77d58de
jjur <juraj.vasek@minerva.kgi.edu>
2021-12-11 02:21:07 +0000

    Registration

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,27 +18,33 @@ class Home extends BaseController
     {
         $DATA["hi"] = "Just making sure data is not empty";
         helper("form"); # Load form validation library
-        $validation = \Config\Services::validation();
-
-        $validation->setRule('name', 'Name', 'required');
-        $validation->setRule('email', 'Email', 'required|valid_email');
-        $validation->setRule('password', 'Password', 'required|min_length[8]');
 
 
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $auth = new IonAuth();
-            if ($validation->run()){
+
+            //$validation = \Config\Services::validation();
+            //$validation->setRule('name', 'Name', 'required');
+            //$validation->setRule('email', 'Email', 'required|valid_email');
+            //$validation->setRule('password', 'Password', 'required|min_length[8]');
+
+            $ready = $this->validate(['name' => 'required|min_length[2]',
+                'email' => 'required|valid_email',
+                'password' => 'required|min_length[8]']);
+
+
+            if ($ready){
             $name = $this->request->getVar("name", FILTER_SANITIZE_STRING);
             $email = $this->request->getVar("email", FILTER_SANITIZE_STRING);
             $password = $this->request->getVar("password", FILTER_SANITIZE_STRING);
             $result = $auth->register($email,$password,$email,array("first_name"=>$name));
             if ($result){
-                redirect("Auth/Login");
+                redirect()->to(base_url()."Auth/Login");
             }else{
                 $DATA["message"] = "Registration Failed, User already exists.";
             }
             }else{
-                $DATA["message"] = $validation->listErrors();
+                $DATA["message"] = $this->validator->listErrors();
             }
 
 
