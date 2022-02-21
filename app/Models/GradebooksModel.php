2e17a46915310e66079a697a9259db99d1b34d3a
jjur <juraj.vasek@minerva.kgi.edu>
2022-02-21 07:34:41 +0100

    Expanded gradebook model with more options for working files

--- app/Models/GradebooksModel.php
+++ app/Models/GradebooksModel.php
@@ -14,11 +14,49 @@ class GradebooksModel extends Model
     protected $deletedField  = 'g_deleted_at';
     protected $primaryKey = 'g_id';
 
-    public function get_all_for_assignment_id($assignment_id): array
+
+    /**
+     * Returns full history of gradebooks associated with specified Assignment ID.
+     * @param $assignment_id
+     * @return array
+     */
+    public function get_all_for_assignment_id($assignment_id)
     {
         return $this->asArray()
             ->where(['g_assignment' => $assignment_id])
             ->orderBy("g_created_at","DESC")
+            ->join("files","files.f_id = g_file")
             ->findAll();
     }
+
+    /**
+     * Returns last 5 uploaded gradebook records associated
+     * with specified Assignment ID.
+     * @param $assignment_id
+     * @return array
+     */
+    public function get_5_for_assignment_id($assignment_id)
+    {
+        return $this->asArray()
+            ->where(['g_assignment' => $assignment_id])
+            ->orderBy("g_created_at","DESC")->limit(5)
+            ->join("files","files.f_id = g_file")
+            ->findAll();
+    }
+
+    /**
+     * Returns most recent gradebook records associated
+     * with specified Assignment ID.
+     * @param $assignment_id
+     * @return array
+     */
+    public function find_latest_for_assignment($assignment_id)
+    {
+        return $this->asArray()
+            ->where(['g_assignment' => $assignment_id])
+            ->orderBy("g_created_at","DESC")
+            ->join("files","files.f_id = g_file")
+            ->first();
+    }
+
 }
