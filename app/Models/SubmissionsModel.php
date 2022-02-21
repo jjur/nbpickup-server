8f9501d9980a4ffd774cec857efa6323bf482d3e
jjur <juraj.vasek@minerva.kgi.edu>
2022-02-21 17:03:20 +0100

    Added Grade together with the submissions

--- app/Models/SubmissionsModel.php
+++ app/Models/SubmissionsModel.php
@@ -16,7 +16,7 @@ class SubmissionsModel extends Model
 
     public function get_submissions($assignment_id){
         #$db = db_connect();
-        $query = $this->db->query("SELECT username, f_filename_internal, f_filename_original, f_created_at, f_updated_at FROM submissions JOIN users ON users.id=submissions.s_user JOIN file_submission ON file_submission.submission = submissions.s_id JOIN files ON files.f_id = file_submission.file WHERE (s_id IN (SELECT MAX(s_id) as s_id FROM submissions WHERE (`s_assignment` = $assignment_id) GROUP BY `s_user`, `s_assignment`))");#->result_array();
+        $query = $this->db->query("SELECT username, f_filename_internal, f_filename_original, f_created_at, f_updated_at, g_score FROM submissions JOIN users ON users.id=submissions.s_user JOIN file_submission ON file_submission.submission = submissions.s_id JOIN files ON files.f_id = file_submission.file LEFT JOIN grades ON submissions.s_id = grades.g_submission WHERE (s_id IN (SELECT MAX(s_id) as s_id FROM submissions WHERE (`s_assignment` = $assignment_id) GROUP BY `s_user`, `s_assignment`))");#->result_array();
         return $query->getResultArray();
     }
 }
